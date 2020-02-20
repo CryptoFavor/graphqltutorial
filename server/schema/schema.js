43e214dfe9be1878f39ce72d354ac1fcbbf0e1d7
FAVOR AI LLC <favorco@protonmail.com>
2020-02-20 01:08:10 -0500

    fixes missing new keywork

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,9 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+let books = [{id: '1', name: 'a', genre: 'genreX'}, {id: '2', name: 'b', genre: 'genreX'}, {id: '3', name: 'c', genre: 'genreY'}];
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -11,7 +14,7 @@ const BookType = new GraphQLObjectType({
     })
 });
 
-const RootQuery = GraphQLObjectType({
+const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
         book: {
@@ -23,6 +26,7 @@ const RootQuery = GraphQLObjectType({
             },
             resolve(parent, args) {
                 //code to get data from db
+                return _.find(books, {id: args.id});
             }
         }
     }
