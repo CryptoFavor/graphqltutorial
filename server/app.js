383872ee4089775e8cd28944eca95be0fb2471fd
FAVOR AI LLC <favorco@protonmail.com>
2020-02-20 00:38:49 -0500

    adds first schemas and requires graphql modules

--- server/app.js
+++ server/app.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+
 const port = 4000;
 
 const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    schema
+}));
+
+
 
 app.listen(port, () => {console.log(`Server listening on port ${port}...`)});
